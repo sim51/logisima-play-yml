68cd8cef06a723372fcbda7646a7a37de9d50980
Benoit Simard <github@bsimard.com>
2010-06-12 00:33:16 +0200

    - add play command - add jpa support

--- commands.py
+++ commands.py
@@ -1,15 +1,33 @@
-# Here you can create play commands that are specific to the module
+# -*- coding: utf-8 -*-
+# Here you can create play commands that are specific to the module, and extend existing commands
+import os, os.path
+import getopt
+import sys
+import subprocess
 
-# Example below:
-# ~~~~
-if play_command == 'logisima-play-yml:hello':
-	try:
-		print "~ Hello from logisima-play-yml"
-		sys.exit(0)
-				
-	except getopt.GetoptError, err:
-		print "~ %s" % str(err)
-		print "~ "
-		sys.exit(-1)
-		
-	sys.exit(0)
+if play_command == 'yml:help':
+        print "~ Help for logisima-yml module"
+        print "~ "
+        print
+        sys.exit(0)
+
+if play_command == "yml:generate":
+        print "~ Generating yml from the database"
+        print "~ "
+        check_application()
+        load_modules()
+        do_classpath()
+        try:
+            # This is the new style to get the extra arg
+            do_java('play.modules.yml.YmlExtractor', sys.argv)
+        except Exception:
+            # For play! < 1.0.3
+            do_java('play.modules.yml.YmlExtractor')
+        try:
+            subprocess.call(java_cmd, env=os.environ)
+        except OSError:
+            print "Could not execute the java executable, please make sure the JAVA_HOME environment variable is set properly (the java executable should reside at JAVA_HOME/bin/java). "
+            sys.exit(-1)
+        print
+        sys.exit(0)
+
